f05bf899838a829b464a087b4caca2819340f929
kociolekl <lukasz.kociolek@ttpsc.com>
2026-02-09 12:45:51 +0000

    Add management command to populate octofit_db with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from datetime import date
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='marvel', description='Marvel Team')
+        dc = Team.objects.create(name='dc', description='DC Team')
+
+        # Create users
+        tony = User.objects.create(name='Tony Stark', email='tony@marvel.com', team='marvel')
+        steve = User.objects.create(name='Steve Rogers', email='steve@marvel.com', team='marvel')
+        bruce = User.objects.create(name='Bruce Wayne', email='bruce@dc.com', team='dc')
+        clark = User.objects.create(name='Clark Kent', email='clark@dc.com', team='dc')
+
+        # Create activities
+        Activity.objects.create(user=tony, activity_type='run', duration=30, date=date(2024, 1, 1))
+        Activity.objects.create(user=steve, activity_type='cycle', duration=45, date=date(2024, 1, 2))
+        Activity.objects.create(user=bruce, activity_type='swim', duration=60, date=date(2024, 1, 3))
+        Activity.objects.create(user=clark, activity_type='yoga', duration=20, date=date(2024, 1, 4))
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=150)
+        Leaderboard.objects.create(team=dc, points=120)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do pushups', difficulty='easy')
+        Workout.objects.create(name='Squats', description='Do squats', difficulty='medium')
+        Workout.objects.create(name='Plank', description='Hold plank', difficulty='hard')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
