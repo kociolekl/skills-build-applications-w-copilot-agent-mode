aae766eb48055f296dc10f60997bfe22e9584c8d
kociolekl <lukasz.kociolek@ttpsc.com>
2026-02-09 12:49:11 +0000

    Refactor settings and add API root view to support dynamic base URL for codespaces

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+
+import os
 from octofit_tracker import views as app_views
+
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    base_url = f"https://{codespace_name}-8000.app.github.dev/api" if codespace_name else "/api"
+    return Response({
+        'users': f'{base_url}/users/',
+        'teams': f'{base_url}/teams/',
+        'activities': f'{base_url}/activities/',
+        'leaderboard': f'{base_url}/leaderboard/',
+        'workouts': f'{base_url}/workouts/',
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', app_views.api_root, name='api-root'),
+    path('', api_root, name='api-root'),
     path('', include('octofit_tracker.urls')),
 ]
